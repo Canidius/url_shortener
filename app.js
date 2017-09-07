976a8c4550854226222eae4fc83ad4e816df0c5d
Egor Naviznev <ego-nav@yandex.ru>
2017-09-07 17:58:09 +0300

    add mongoose and request to package.json and used it in project. Made api for url shortening.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var config = require('./config');
+
+mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
 
 var app = express();
 
@@ -17,7 +21,7 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,61 @@
+var config = require('../config');
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+var Url = require('../models/url');
+
+router.get('/:shortenedUrl', function (req, res, next) {
+  Url.findOne({short_url: req.params.shortenedUrl}, function (err, doc) {
+    if (!doc) {
+      return  res.redirect(config.webhost);
+    }
+
+    return res.redirect(doc.long_url);
+  });
+});
+
+
+router.post('/api/shorten', function (req, res, next) {
+  var longUrl = req.body.longUrl;
+  console.log(longUrl);
+  request.get(longUrl, function(err, resp, body) {
+    if(err) return res.send({ error: true, message: 'Url is not valid' });
+    Url.findOne({ long_url: longUrl }, function (err, doc){
+      if (doc){
+        return res.send({shortUrl: config.webhost + doc.short_url});
+      }
+      var newUrl = Url({
+        long_url: longUrl
+      });
+      newUrl.save(function(err) {
+        if (err){
+          console.log(err);
+        }
+        return res.send({ shortUrl: config.webhost + newUrl.short_url });
+      });
+    });
+  });
+});
+
+router.post('/api/shorten_desired', function (req, res, next) {
+  var shortUrl = req.body.shortUrl;
+  var longUrl = req.body.longUrl;
+
+  Url.findOne({ short_url: shortUrl }, function (err, doc){
+    if (doc){
+      return res.send({ shortUrl: config.webhost + doc.short_url });
+    }
+    Url.findOneAndUpdate({ long_url: longUrl }, { short_url: shortUrl }, function(err, doc){
+      res.send({ shortUrl: config.webhost + shortUrl });
+    })
+  });
+
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index');
 });
 
 module.exports = router;
